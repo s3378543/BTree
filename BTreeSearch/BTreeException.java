458392a8675ad171bb842d8caeeaf07fb240780d
Callum Pullyblank <s3378543@student.rmit.edu.au>
2017-05-20 18:13:35 +1000

    Create BTreeException.java

---
+++ BTreeSearch/BTreeException.java
@@ -0,0 +1,10 @@
+public class BTreeException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public BTreeException() {
+		
+	}
+	
+	public BTreeException(String message) {
+		super(message);
+	}
+}
